14e6f7ec4092f9c138caa7386a4a0ad2965d0090
Robin Dadswell <19610103+RobinDadswell@users.noreply.github.com>
2024-09-11 12:15:03 +0100

    New: SharePoint Site reporting script

---
+++ M365Reporting/Public/Export-M365RSharePointUsageReport.ps1
@@ -0,0 +1,178 @@
+function Export-M365RSharePointUsageReport
+{
+    <#
+        .SYNOPSIS
+            Generates a basic usage report for SharePoint sites (not including OneDrive)
+
+        .DESCRIPTION
+            Generates a basic usage report for SharePoint sites (not including OneDrive)
+            The report will contain the following information:
+            - Owner (UPN)
+            - CurrentUsage (GB)
+            - PercentageOfQuotaUsed
+            - Quota (GB)
+            - QuotaWarning (GB)
+            - QuotaType
+            - LastModified
+            - URL
+            - Status
+
+        .PARAMETER TenantName
+            The Tenant name, like contoso if the tenant is contoso.onmicrosoft.com
+            vanity names, e.g. contoso.com, are NOT supported!
+
+        .PARAMETER OutputPath
+            The folder path that you would like the xlsx output to be placed
+
+        .PARAMETER NoStatisticsReport
+            This stops the statistics report summary page from being created
+
+        .NOTES
+            Quick and dirty implementation to generate a simple XLSX report file
+            This script was sourced https://hochwald.net/post/powershell-usage-reporting-onedrive-business/ and has been updated to reflect our requirements
+    #>
+    [CmdletBinding(ConfirmImpact = 'None')]
+    param
+    (
+        [Parameter(
+            Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript(
+            {
+                if ($_ -like '*.*')
+                {
+                    throw 'This should be the tenant name e.g. Contoso from contoso.onmicrosoft.com'
+                }
+                else
+                {
+                    return $true
+                }
+            }
+        )]
+        [Alias('Tenant', 'M365Name', 'M365TenantName')]
+        [string]
+        $TenantName,
+        [Parameter(Mandatory)]
+        [string]
+        $ClientId,
+        [Parameter()]
+        [Alias('Device', 'DeviceAuthentication')]
+        [switch]
+        $DeviceLogin,
+        [Parameter(
+            Mandatory
+        )]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript(
+            {
+                if (!(Test-Path -Path $_))
+                {
+                    throw "The folder $_ does not exist"
+                }
+                else
+                {
+                    return $true
+                }
+            })]
+        [IO.DirectoryInfo]
+        $OutputPath,
+        [switch]$NoStatisticsReport
+    )
+
+    $paramOneDriveUsageReport = @{
+        TenantName = $TenantName
+    }
+
+    if ($DeviceLogin)
+    {
+        $paramOneDriveUsageReport['DeviceLogin'] = $true
+    }
+
+    $report = Get-M365ROneDriveUsageReport -TenantName $TenantName -ClientId $ClientId
+
+    $outputFile = $OutputPath.FullName.TrimEnd([System.IO.Path]::DirectorySeparatorChar) + [System.IO.Path]::DirectorySeparatorChar + (Get-Date -Format yyyyMMdd_HHmmss) + '-' + $TenantName + '-' + 'OneDriveUsageReport.xlsx'
+
+    if ($report.Count -ge 1)
+    {
+        # Export the CSV Report
+        try
+        {
+            $paramExportExcel = @{
+                Path                    = $outputFile
+                WorksheetName           = 'OneDriveUsageReport'
+                ErrorAction             = 'Stop'
+                FreezeTopRowFirstColumn = $true
+                AutoSize                = $true
+                AutoFilter              = $true
+            }
+            $report | Sort-Object -Property CurrentUsage -Descending | Export-Excel @paramExportExcel
+            if (-not $NoStatisticsReport)
+            {
+                $paramExportStatistics = @{
+                    Path                    = $outputFile
+                    WorksheetName           = 'OneDriveUsageStatistics'
+                    ErrorAction             = 'Stop'
+                    FreezeTopRowFirstColumn = $true
+                    AutoSize                = $true
+                    AutoFilter              = $true
+                    Numberformat            = 'Percentage'
+                    MoveToStart             = $true
+                }
+
+                $reportCount = $report.Count
+                $reportStatistics = New-Object System.Collections.Generic.List[System.Object]
+                $usageHashTable = @{
+                    Statistic             = 'Less than 1GB Utilisation'
+                    'Percentage of Users' = [Math]::Round((($report.CurrentUsage | Where-Object { $_ -lt 1 }).Count / $reportCount), 4)
+                }
+
+                $reportStatistics.Add([PSCustomObject]$usageHashTable) | Out-Null
+
+                foreach ($numberOfDays in 30, 60, 90)
+                {
+                    $usageHashTable = @{
+                        Statistic             = "Content not modified within the last $numberOfDays days"
+                        'Percentage of Users' = [Math]::Round((($report.LastModifiedDate | Where-Object { (Get-Date $_) -lt (Get-Date).AddDays(-$numberOfDays) }).Count / $reportCount), 4)
+                    }
+                    $reportStatistics.Add([PSCustomObject]$usageHashTable) | Out-Null
+                }
+
+                $reportStatistics | Select-Object Statistic, 'Percentage of Users' | Export-Excel @paramExportStatistics
+            }
+
+        }
+        catch
+        {
+            #region ErrorHandler
+            # get error record
+            [Management.Automation.ErrorRecord]$e = $_
+
+            # retrieve information about runtime error
+            $info = [PSCustomObject]@{
+                Exception = $e.Exception.Message
+                Reason    = $e.CategoryInfo.Reason
+                Target    = $e.CategoryInfo.TargetName
+                Script    = $e.InvocationInfo.ScriptName
+                Line      = $e.InvocationInfo.ScriptLineNumber
+                Column    = $e.InvocationInfo.OffsetInLine
+            }
+
+            # output information. Post-process collected info, and log info (optional)
+            $info | Out-String | Write-Verbose
+
+            $paramWriteError = @{
+                Message      = $e.Exception.Message
+                ErrorAction  = 'Stop'
+                Exception    = $e.Exception
+                TargetObject = $e.CategoryInfo.TargetName
+            }
+            Write-Error @paramWriteError
+            #endregion ErrorHandler
+        }
+        Write-Output "Please find the report $outputFile"
+    }
+    else
+    {
+        Write-Output "There were no SharePoint Sites to report on"
+    }
+}
---
+++ M365Reporting/Public/Get-M365RSharePointUsageReport.ps1
@@ -0,0 +1,186 @@
+function Get-M365RSharePointUsageReport
+{
+    <#
+        .SYNOPSIS
+            Generates a basic usage report for SharePoint sites (not including OneDrive)
+
+        .DESCRIPTION
+            Generates a basic usage report for SharePoint sites (not including OneDrive)
+            The report will contain the following information:
+            - Owner (UPN)
+            - CurrentUsage (GB)
+            - PercentageOfQuotaUsed
+            - Quota (GB)
+            - QuotaWarning (GB)
+            - QuotaType
+            - LastModified
+            - URL
+            - Status
+
+        .PARAMETER TenantName
+            The Tenant name, like contoso if the tenant is contoso.onmicrosoft.com
+            vanity names, e.g. contoso.com, are NOT supported!
+
+        .NOTES
+            This script was sourced https://hochwald.net/post/powershell-usage-reporting-onedrive-business/ and has been updated to reflect our requirements
+    #>
+    [CmdletBinding(ConfirmImpact = 'None')]
+    param
+    (
+        [Parameter(ValueFromPipeline,
+            ValueFromPipelineByPropertyName,
+            Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [ValidateScript(
+            {
+                if ($_ -like '*.*')
+                {
+                    throw 'This should be the tenant name e.g. Contoso from contoso.onmicrosoft.com'
+                }
+                else
+                {
+                    return $true
+                }
+            }
+        )]
+        [Alias('Tenant', 'M365Name', 'M365TenantName')]
+        [string]
+        $TenantName,
+        [Parameter(Mandatory)]
+        [string]
+        $ClientId,
+        [Parameter()]
+        [Alias('Device', 'DeviceAuthentication')]
+        [switch]
+        $DeviceLogin
+    )
+
+    process
+    {
+        # Garbage Collection
+        [GC]::Collect()
+
+
+        # Create the Connection URI
+        $AdminURL = ('https://' + $TenantName + '-admin.sharepoint.com')
+
+        #TODO: Better cross platform authentication support (e.g. for headless environments etc.)
+        # Connect to SharePoint Online
+        $paramConnectPnPPowerShell = @{
+            URL         = $AdminURL
+            ErrorAction = 'Stop'
+            Interactive = $true
+            ClientId    = $ClientId
+        }
+
+        if ($DeviceLogin)
+        {
+            $paramConnectPnPPowerShell['DeviceLogin'] = $true
+        }
+
+        try
+        {
+            Get-PnPTenant -ErrorAction Stop | Out-Null
+            $preConnected = $true
+            Write-Verbose 'Previously connected to SPO Service, no extra authentication to occur'
+        }
+        catch
+        {
+            Write-Verbose 'Not Connected to SPO Service, connecting to SPO Service'
+            Connect-PnPOnline @paramConnectPnPPowerShell | Out-Null
+        }
+        try
+        {
+            Get-PnPTenant -ErrorAction Stop | Out-Null
+        }
+        catch
+        {
+            return
+        }
+
+
+        # Create new object
+        $Report = New-Object System.Collections.Generic.List[System.Object]
+
+        $paramGetO4BSites = @{
+            IncludeOneDriveSites = $true
+            Filter               = "Url -notlike '-my.sharepoint.com/personal/'"
+            ErrorAction          = 'SilentlyContinue'
+        }
+        $sites = (Get-PnPTenantSite @paramGetO4BSites)
+
+        foreach ($site in $sites)
+        {
+            try
+            {
+                # Cleanup
+                $StatsReport = $null
+
+                # Create the Reporting object
+                $StatsReport = [PSCustomObject]@{
+                    Owner                   = $site.Owner
+                    CurrentUsage            = '{0:F3}' -f ($site.StorageUsageCurrent / 1024) -as [decimal]
+                    PercentageOfQuotaUsed   = ($site.StorageUsageCurrent / $site.StorageQuota) -as [decimal]
+                    Quota                   = '{0:F0}' -f ($site.StorageQuota / 1024) -as [int]
+                    QuotaWarning            = '{0:F0}' -f ($site.StorageQuotaWarningLevel / 1024) -as [int]
+                    QuotaType               = $site.StorageQuotaType
+                    LastModifiedDate        = $site.LastContentModifiedDate.ToLongDateString()
+                    URL                     = $site.URL
+                    IsTeamsChannelConnected = $site.IsTeamsChannelConnected
+                    IsTeamsConnected        = $site.IsTeamsConnected
+                    IsHubSite               = $site.IsHubSite
+                    Status                  = $site.Status
+                }
+
+                # Append the report
+                $Report.Add($StatsReport)
+            }
+            catch
+            {
+                #region ErrorHandler
+                # get error record
+                [Management.Automation.ErrorRecord]$e = $_
+
+                # retrieve information about runtime error
+                $info = [PSCustomObject]@{
+                    Exception = $e.Exception.Message
+                    Reason    = $e.CategoryInfo.Reason
+                    Target    = $e.CategoryInfo.TargetName
+                    Script    = $e.InvocationInfo.ScriptName
+                    Line      = $e.InvocationInfo.ScriptLineNumber
+                    Column    = $e.InvocationInfo.OffsetInLine
+                }
+
+                # output information. Post-process collected info, and log info (optional)
+                $info | Out-String | Write-Verbose
+
+                Write-Warning -Message $info.Exception
+                #endregion ErrorHandler
+            }
+        }
+    }
+    end
+    {
+        # Disconnect from SharePoint Online
+        try
+        {
+            if (-not $preConnected)
+            {
+                Write-Verbose 'Not previously connected to SPO Service, disconnecting from SPO'
+                (Disconnect-PnPOnline -ErrorAction SilentlyContinue) | Out-Null
+            }
+            else
+            {
+                Write-Verbose 'Previously connected to SPO, will remain connected to SPO'
+            }
+        }
+        catch
+        {
+            [GC]::Collect()
+        }
+
+        # Garbage Collection
+        [GC]::Collect()
+        return $Report
+    }
+}
